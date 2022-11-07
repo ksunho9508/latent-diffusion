addd0f69071f4d4155ad62d37cb9c6729b1e5d4e
SUNHO <ksunho9508>
2022-11-07 18:26:42 +0900

    간략화

--- scripts/train_autoencoder.sh
+++ scripts/train_autoencoder.sh
@@ -1,4 +1,4 @@
-python main.py --base configs/autoencoder/autoencoder_vq_64x64x3.yaml -t --gpus 0,1,2,3 &&
-python main.py --base configs/autoencoder/autoencoder_vq_32x32x4.yaml -t --gpus 0,1,2,3&&
-python main.py --base configs/autoencoder/autoencoder_vq_16x16x16.yaml -t --gpus 0,1,2,3 &&
-python main.py --base configs/autoencoder/autoencoder_vq_8x8x64.yaml -t --gpus 0,1,2,3
+python main.py --base configs/autoencoder/autoencoder_vq_64x64x3.yaml -t --no-test --gpus 0,1,2,3 &&
+python main.py --base configs/autoencoder/autoencoder_vq_32x32x4.yaml -t --no-test --gpus 0,1,2,3 &&
+python main.py --base configs/autoencoder/autoencoder_vq_16x16x16.yaml -t --no-test --gpus 0,1,2,3 &&
+python main.py --base configs/autoencoder/autoencoder_vq_8x8x64.yaml -t --no-test --gpus 0,1,2,3
