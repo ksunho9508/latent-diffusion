195b07e6388aa3a7284a2b39b731b4a5bebdbf98
SUNHO <ksunho9508>
2022-11-07 18:25:39 +0900

    간략화

--- parser.py
+++ parser.py
@@ -76,7 +76,7 @@ def get_parser(**parser_kwargs):
         "-s",
         "--seed",
         type=int,
-        default=23,
+        default=2022,
         help="seed for seed_everything",
     )
     parser.add_argument(
